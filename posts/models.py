a5515758707c2b7eaf66b14bca0a20ce05ed6ab4
MarjonaDavlyatova <marjona.davlyatova.h@gmail.com>
2022-10-17 22:41:55 +0500

    masters

--- posts/models.py
+++ posts/models.py
@@ -1,12 +1,19 @@
 from pydoc import describe
 from unittest.util import _MAX_LENGTH
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField(max_length=500)
+    # created_date = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         return f"Post {self.title}"
+    
