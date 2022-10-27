6264873e3a50017cc625e7560ee105e82d50719e
MarjonaDavlyatova <marjona.davlyatova.h@gmail.com>
2022-10-27 18:47:36 +0500

    post_list

--- posts/models.py
+++ posts/models.py
@@ -2,15 +2,21 @@ from django.db import models
 from django.utils import timezone
 
 
-
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField(max_length=500)
-    created_date = models.DateTimeField(default=timezone.now)
-    # created_at = models.DateTimeField(auto_now_add=True)
-    # updated_at = models.DateTimeField(auto_now=True)
-
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    is_draft = models.BooleanField(default=True)
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    published_at = models.DateTimeField(null=True, blank=True)
 
     def __str__(self):
-        return f"Post {self.title}"
-    
+        return f"{self.title:.30}"
+
+    def save(self, *args, **kwags):
+        if not self.is_draft and not self.published_at:
+            self.published_at = timezone.now()
+
+
+        super().save(*args, **kwags)
